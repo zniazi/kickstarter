2d1c8f7d9d204a90b8b05de08c8697d5bcae75c2
Zak Niazi <zniazi1029@gmail.com>
2014-08-26 17:58:03 -0400

    Modify project.rb file.

--- app/models/project.rb
+++ app/models/project.rb
@@ -16,21 +16,20 @@ class Project < ActiveRecord::Base
   validates :category_id, :goal, :location_id, :title, :blurb, :end_date,
     :description, :challenges, presence: true
   validates :goal, numericality: { greater_than: 0, less_than_or_equal_to: 100_000_000 }
-  validates_presence_of: :rewards
+  validates_presence_of :rewards
   validate :user_name, :user_biography
 
   private
   def user_name
-    if (current_user.name.nil?)
+    if current_user.name.nil?
       errors.base << "Creator name is required."
-    end
     elsif (current_user.name.length < 1)
       errors.base << "Creator name is too short (minimum is 1 characters)"
     end
   end
 
   def user_biography
-    if (current_user.biography.nil?)
+    if current_user.biography.nil?
       errors.base << "Biography is required."
     end
   end
