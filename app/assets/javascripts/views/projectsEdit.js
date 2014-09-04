a6da40c35a8a0b183b571a77cd81c3d4969b1705
Zak Niazi <zniazi1029@gmail.com>
2014-09-03 23:28:35 -0400

    Add icon, begin methods to render multiple views for edit page in backbone.

--- app/assets/javascripts/views/projectsEdit.js
+++ app/assets/javascripts/views/projectsEdit.js
@@ -1,8 +1,24 @@
 App.Views.ProjectsEdit = Backbone.View.extend({
-  template: JST["project/edit"],
+  template: function () {		
+  	switch(parseInt(this._view)) {
+		case 1:
+			console.log(1);
+			return JST["projects/edit-basics"];
+			break;
+		case 2:
+			console.log(2);
+			return JST["projects/edit-rewards"];
+			break;
+		default:
+			return JST["projects/edit-basics"];
+			break;
+  	}
+  },
+	
   events: {
     "submit form": "submit",
-    "change #product_category": "subcategoryRender"
+    "change #product_category": "renderSubcategory",
+		"click .edit-nav": "renderEdit"
   },
 
   initialize: function () {
@@ -10,10 +26,11 @@ App.Views.ProjectsEdit = Backbone.View.extend({
     App.Collections.locations.fetch();
     this.listenTo(App.Collections.categories, "sync", this.render);
     this.listenTo(App.Collections.locations, "sync", this.render);
+		this._view = 2;
   },
 
   render: function () {
-    var renderedContent = this.template({
+    var renderedContent = this.template()({
       project: this.model, primeCategory: this.model.category(),
       primaryCategories: App.Collections.categories, locations: App.Collections.locations
     });
@@ -29,7 +46,7 @@ App.Views.ProjectsEdit = Backbone.View.extend({
     this.model.update(params["project"]);
   },
 
-  subcategoryRender: function (event) {
+  renderSubcategory: function (event) {
     var newId = $(event.currentTarget).val();
     var primeCategory = App.Collections.categories.get(newId)
     var renderedContent = this.template({
@@ -39,5 +56,16 @@ App.Views.ProjectsEdit = Backbone.View.extend({
     this.$el.html(renderedContent);
 
     return this;
-  }
+  },
+	
+	renderEdit: function (event) {
+		event.preventDefault();
+    this._view = $(event.target).attr("data-id");
+		this.render();
+	}
 });
+
+
+
+
+
